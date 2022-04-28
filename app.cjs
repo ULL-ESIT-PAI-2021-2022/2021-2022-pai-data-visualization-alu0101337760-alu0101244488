14ec22fc2ac75f76a213907bfd66228a5ffe92de
Enrique Viña Alonso <alu0101337760@ull.edu.es>
2022-04-28 20:58:32 +0000

    Added basic testing files and the app.cjs script to deploy the webpage

---
+++ app.cjs
@@ -0,0 +1,19 @@
+const express = require('express');
+const PATH = require('path');
+
+const APP = express();
+
+// set the port
+APP.set('port', 8080);
+
+// tell express that we want to use the www folder
+// for our static assets
+
+APP.use(express.static("www"));
+
+// Listen for requests
+const SERVER = APP.listen(APP.get('port'), '0.0.0.0', function() {
+  console.log(
+      'The server is running on http://10.6.129.150:' +
+      APP.get('port'));
+});
