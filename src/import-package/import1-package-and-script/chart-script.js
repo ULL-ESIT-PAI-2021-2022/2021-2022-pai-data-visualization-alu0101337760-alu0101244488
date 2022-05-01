02ecfaf8240baebe07f9588a26fee2c51223a815
Enrique Viña Alonso <alu0101337760@ull.edu.es>
2022-05-01 10:43:26 +0000

    modified import example

--- src/import-package/import1-package-and-script/chart-script.js
+++ src/import-package/import1-package-and-script/chart-script.js
@@ -7,7 +7,7 @@
  * @author Aram Pérez Dios <aram.perez.dios.04@ull.edu.es>
  * @author Enrique Viña Alonso <enrique.vina.29@ull.edu.es>
  * @since Apr 28 2022
- * 
+ *
  * @desc Example of a line chart to show the basic structure
  *       of the Chart object of the Chart.js library
  * @see {@link https://github.com/ULL-ESIT-PAI-2021-2022/2021-2022-pai-data-visualization-alu0101337760-alu0101244488}
@@ -16,76 +16,45 @@
 
 'use strict';
 
-/** @desc Data to to fill the chart */
+/** @desc Context of html element */
+const CTX = document.getElementById('myChart').getContext('2d');
 
-/** @desc Structure of the dataset for the chart ot display */
-const DATA = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-  datasets: [{
-    label: 'Looping tension',
-    data: [65, 59, 80, 81, 26, 55, 40],
-    fill: false,
-    borderColor: 'rgb(75, 192, 192)',
-  }]
-};
-
-
-
-/** @desc Configuration of the chart */
-
-/** @desc Type of chart to represent */
-const TYPE = 'line';
-
-/** @desc Configuration of the animation of the chart */
-const ANIMATION = {
-  tension: {
-    duration: 1000,
-    easing: 'linear',
-    from: 0.3,
-    to: 0.1,
-    loop: true
-  }
-};
-
-/** @desc Configuration of the scale of the chart */
-const SCALE = {
-  y: {
-    min: 0,
-    max: 100
-  }
-};
-
-/** @desc Plugins allows to override the default attributes of the chart */
-const PLUGINS = {
-  legend: {
-    labels: {
-      font: {
-        size: 20
+/** @desc create chart with context and configuration */
+let myChart = new Chart(CTX, {
+  type: 'line',
+  data: {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [{
+      label: 'Looping tension',
+      data: [65, 59, 80, 81, 26, 55, 40],
+      fill: false,
+      borderColor: 'rgb(75, 192, 192)',
+    }]
+  },
+  options: {
+    plugins: {
+      legend: {
+        labels: {
+          font: {size: 20},
+        },
+      },
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+    animations: {
+      tension: {
+        duration: 500,
+        easing: 'linear',
+        from: 0.5,
+        to: 0.1,
+        loop: true,
+      }
+    },
+    scales: {
+      y: {
+        min: 0,
+        max: 100,
       }
     }
   }
-};
-
-/** @desc Option for the chart storing all the configurations */
-const OPTIONS = {
-  plugins: PLUGINS,
-  responsive: true,
-  maintainAspectRatio: false,
-  animations: ANIMATION,
-  scales: SCALE
-};
-
-/** @desc Structure of the configuration and format of the chart */
-const CONFIG = {
-  type: TYPE,
-  data: DATA,
-  options: OPTIONS
-};
-
-
-
-/** @desc Context of html element */
-const CTX = document.getElementById("myChart").getContext("2d");
-
-/** @desc create chart with context and configuration */
-let myChart = new Chart(CTX, CONFIG);
+});
--- src/import-package/import2-splitted-script/config.js
+++ src/import-package/import2-splitted-script/config.js
@@ -7,10 +7,10 @@
  * @author Aram Pérez Dios <aram.perez.dios.04@ull.edu.es>
  * @author Enrique Viña Alonso <enrique.vina.29@ull.edu.es>
  * @since Apr 28 2022
- * 
+ *
  * @desc Export configuration for the bar chart example where
  *       the data is in other file
- *       
+ *
  * @see {@link https://github.com/ULL-ESIT-PAI-2021-2022/2021-2022-pai-data-visualization-alu0101337760-alu0101244488}
  * @license GPLv3
  */
@@ -18,9 +18,9 @@
 'use strict';
 
 /** @desc import data object */
-import { DATA } from './setup.js';
+import {DATA} from './setup.js';
 
-/** 
+/**
  * @desc Type of chart to represent
  * @see {@link https://https://www.chartjs.org/docs/latest/} to see all avalible types
  */
@@ -30,8 +30,5 @@ const TYPE = 'bar';
 export const CONFIG = {
   type: TYPE,
   data: DATA,
-  options: {
-    responsive: true,
-    maintainAspectRatio: false
-  }
+  options: {responsive: true, maintainAspectRatio: false}
 };
